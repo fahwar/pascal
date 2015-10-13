3670ef19b300af83bd5628403c9183684c15d9aa
Fahmi Anwar <fahwar@live.com>
2015-10-13 18:48:07 +0700

    Create pascal03.pas

---
+++ dasar/pascal03.pas
@@ -0,0 +1,9 @@
+Program Tulis_alamat;
+Begin
+     writeln('Teknik Informatika');
+     write('Fakultas Teknik');
+     writeln('Universitas Siliwangi');
+     writeln;
+     write('Jl. Siliwangi No 24 Tasikmalaya');
+     readln;
+End.
