c863ec551208940879f65f4c2337a23cc4e23086
Fahmi Anwar <fahwar@live.com>
2015-10-13 18:48:28 +0700

    Create pascal04.pas

---
+++ dasar/pascal04.pas
@@ -0,0 +1,10 @@
+Program data_mahasiswa;
+Begin
+     writeln('Program Data Mahasiswa');
+     write('Nama : Fahmi Anwar');
+     writeln('');
+     write('NPM : 147006092');
+     writeln(' ');
+     writeln('Kelas : B-2014');
+     readln;
+End.
