0e9905a2803070d838e8d2fde9cbd4ee3f855736
Fahmi Anwar <fahwar@live.com>
2015-10-13 18:48:53 +0700

    Create pascal05.pas

---
+++ dasar/pascal05.pas
@@ -0,0 +1,14 @@
+Program Piramida_Bintang;
+Uses
+    crt;
+Begin
+     writeln('Program membuat susunan bintang');
+     write('                *');
+     writeln(' ');
+     write('          *     *     *');
+     writeln('  ');
+     write('     *    *     *     *      *');
+     writeln('    ');
+     write('*    *    *     *     *      *      *');
+     readln;
+End.
