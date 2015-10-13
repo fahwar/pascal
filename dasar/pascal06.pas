f6006c97a10a0f97a653ff3416ffeacac7fe703f
Fahmi Anwar <fahwar@live.com>
2015-10-13 18:49:10 +0700

    Create pascal06.pas

---
+++ dasar/pascal06.pas
@@ -0,0 +1,25 @@
+Program Bilangan_hitung;
+Uses
+Crt;
+Var
+A:real;
+Begin
+Clrscr;
+writeln('Program Bilangan Matematika');
+write('120+12');
+write('=');
+write(120+12);
+writeln(' ');
+write('120-12');
+write('=');
+write(120-12);
+writeln(' ');
+write('120*12');
+write('=');
+write(120*12);
+writeln(' ');
+A:=120/12;
+write('120/12');
+write('=' ,a:4:2);
+Readln;
+End.
